cc5ef2584ecc4b190c9140855a4637cf5a282fc7
mitsuki0506 <chocogm05@gmail.com>
2026-08-08 19:16:55 +0900

    ら

--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,23 @@
-const CACHE_NAME = "breakfast-checklist-v3";
+const CACHE_NAME = "breakfast-checklist-v4";
 
-const FILES = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./app.js",
-  "./firebase.js",
-  "./manifest.json"
-];
-
-self.addEventListener("install", event => {
-
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(FILES))
-  );
-
+self.addEventListener("install", () => {
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
-
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
+      Promise.all(keys.map(key => caches.delete(key)))
     )
   );
 
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-
   event.respondWith(
-    fetch(event.request)
-      .catch(() => caches.match(event.request))
+    fetch(event.request, {
+      cache: "no-store"
+    })
   );
-
 });
